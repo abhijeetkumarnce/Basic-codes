ea7caf3fdbaed0880567aa2ab62e572b9179b25b
Abhijeet Kumar <42579892+abhijeetkumarnce@users.noreply.github.com>
2018-09-10 23:20:43 +0530

    Sahil loves GFG

---
+++ sahillovesgfg.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+    {
+	    string s;
+	    int count=0;
+	    cin>>s;
+	    for(int i=0;i<s.size();i++)
+        {
+	        if((i<s.size()-2) && s[i] == 'g' && s[i+1] == 'f' && s[i+2] == 'g')
+            {
+	            count++;
+	        }
+	    }
+	    if(count)
+	    cout<<count<<endl;
+	    else
+	    cout<<"-1"<<endl;
+	}
+	return 0;
+}
